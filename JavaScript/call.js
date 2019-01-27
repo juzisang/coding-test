7109c8867f5ec9252b9a2569409d397158a74eda
juzisang <juziexe@gmail.com>
2019-01-27 15:57:30 +0800

    feat(javascript): add custom apply

--- JavaScript/call.js
+++ JavaScript/call.js
@@ -1,23 +1,20 @@
 Function.prototype.customCallEs6 = function(content, ...args) {
-  if (typeof this !== "function") {
-    throw new Error("is not function");
-  }
+  if (typeof this !== "function") throw new Error("is not function");
   content[`_${this.name}`] = this;
   const res = content[`_${this.name}`](...args);
   delete content[`_${this.name}`];
   return res;
 };
 
-Function.prototype.customCallEs5 = function(content) {
-  if (typeof this !== "function") {
-    throw new Error("is not function");
-  }
-  content["_" + this.name] = this;
+Function.prototype.customCallEs5 = function() {
+  if (typeof this !== "function") throw new Error("is not function");
+  var content = arguments[0];
   var str = "content._" + this.name + "(";
-  for (var i = 1; i < arguments.length; i++) {
-    str += arguments.length - 1 === i ? arguments[i] + ")" : arguments[i] + ",";
-  }
-  var res = eval(str);
+  var i = 1;
+  var res = null;
+  content["_" + this.name] = this;
+  for (i; i < arguments.length; i++) str += arguments[i] + (arguments.length - 1 === i ? ")" : ",");
+  res = eval(str);
   delete content["_" + this.name];
   return res;
 };
