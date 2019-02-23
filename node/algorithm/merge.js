368ebef12fd2a2a2a908204e77c8a78a8a15a61e
juzisang <juziexe@gmail.com>
2019-02-23 20:51:01 +0800

    refactor(algo): marge

--- node/algorithm/merge.js
+++ node/algorithm/merge.js
@@ -5,11 +5,11 @@ function mergeSort(array) {
     return array;
   } else {
     let m = Math.floor(array.length / 2);
-    return sort(mergeSort(array.slice(0, m)), mergeSort(array.slice(m)));
+    return sort1(mergeSort(array.slice(0, m)), mergeSort(array.slice(m)));
   }
 }
 
-function sort(left, right) {
+function sort1(left, right) {
   let i = 0;
   let j = 0;
   let array = [];
@@ -21,6 +21,42 @@ function sort(left, right) {
     }
   }
   return array.concat(left.slice(i), right.slice(j));
+}
+
+function sort2(left, right) {
+  let i = 0;
+  let j = 0;
+  let array = [];
+  for (let k = 0; k < left.length + right.length; k++) {
+    if (i >= left.length) {
+      array.push(right[j++]);
+    } else if (j >= right.length) {
+      array.push(left[i++]);
+    } else if (left[i] < right[j]) {
+      array.push(left[i++]);
+    } else {
+      array.push(right[j++]);
+    }
+  }
+  return array;
+}
+
+function sort3(left, right) {
+  let array = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      array.push(left.shift());
+    } else {
+      array.push(right.shift());
+    }
+  }
+  while (left.length) {
+    array.push(left.shift());
+  }
+  while (right.length) {
+    array.push(right.shift());
+  }
+  return array;
 }
 
 // sortTime({
