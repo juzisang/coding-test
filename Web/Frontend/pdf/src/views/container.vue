f190d148ce012f5e579baa011bfda46c9e01d3a2
luof <luof@tianxiaxinyong.com>
2019-10-30 10:47:25 +0800

    完善导出 pdf demo

--- Web/Frontend/pdf/src/views/container.vue
+++ Web/Frontend/pdf/src/views/container.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="container">
+  <div class="container" :style="{visibility: show ? 'visible' : 'hidden'}" ref="container">
     <div class="prints">
       <div class="btn" @click="handleWindowPrint">调起浏览器</div>
       <div class="btn btn1" @click="handleDownPdf">jsPDF + html2canvas</div>
     </div>
-    <iframe id="container" name="container" src="./index.html#/pdf" frameborder="no" width="100%"></iframe>
+    <iframe ref="iframe" :height="pageHeight" src="./index.html#/pdf" frameborder="no" width="100%"></iframe>
   </div>
 </template>
 
@@ -12,29 +12,49 @@
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 export default {
-  mounted() {
-    window.addEventListener('load', () => {
-      const container = document.querySelector('#container');
-      container.style.height = container.contentWindow.document.body.clientHeight + 'px';
-    });
+  data() {
+    return {
+      show: false,
+      pageHeight: 0
+    }
+  },
+  async mounted() {
+    await this.onLoad()
+    this.show = true
+    this.pageHeight = this.$refs.iframe.contentDocument.body.clientHeight
   },
   methods: {
+    onLoad() {
+      return new Promise((reslove) => window.addEventListener('load', reslove))
+    },
     handleWindowPrint() {
-      document.querySelector('#container').contentWindow.print();
+      this.$refs.iframe.contentWindow.print();
     },
     async handleDownPdf() {
-      const pdf = new jsPDF({ format: 'a4' });
-      const width = pdf.internal.pageSize.getWidth();
-
-      const htmlBody = document.querySelector('#container').contentWindow.document.body;
-
-      const pages = await Promise.all([...htmlBody.querySelectorAll('.PDFPage')].map(ele => html2canvas(ele)));
-
-      pages.forEach(canvas => {
-        pdf.addImage(canvas.toDataURL('image/jpeg', 1.0), 'JPEG', 0, 0, width, (canvas.height * width) / canvas.width, null, 'NONE');
+      const pdf = new jsPDF({ format: 'a4', compress: true })
+      const htmlBody = this.$refs.iframe.contentWindow.document.body
+      const pdfPages = await Promise.all([...htmlBody.querySelectorAll('.PDFPage')])
+      const pdfPage = htmlBody.querySelector('.PDFPage')
+      const pageHeight = pdfPage.clientHeight
+      const pageWidth = pdfPage.clientWidth
+      const scaleBy = 2
+      for (const element of pdfPages) {
+        const canvas = document.createElement('canvas')
+        canvas.width = pageWidth * scaleBy
+        canvas.height = pageHeight * scaleBy
+        canvas.style.width = pageWidth * scaleBy + 'px'
+        canvas.style.height = pageHeight * scaleBy + 'px'
+        const context = canvas.getContext('2d')
+        context.scale(scaleBy, scaleBy)
+        context.font = 'Microsoft YaHei'
+        const canvasData = await html2canvas(element, { canvas, scale: 1 })
+        // document.body.appendChild(canvasData)
+        pdf.addImage(canvasData.toDataURL('image/jpeg', 1.0), 'JPEG', 0, 0, 210, 297, null, 'NONE');
         pdf.addPage();
-      });
-    //   pdf.internal.scaleFactor = 1.33;
+      }
+      if (pdf.internal.pages.length > pdfPages.length) {
+        pdf.deletePage(pdf.internal.pages.length - 1)
+      }
       pdf.save('个人报告.pdf');
     }
   }
